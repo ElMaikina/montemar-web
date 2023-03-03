f8cde83df0a8d4c5dee308ddbecac7d48f55f6a9
ElMaikina <mikesoto890@gmail.com>
2023-03-03 12:18:52 -0400

    tuve que reiniciar git xd

---
+++ home.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<center>
+<br>
+<br>
+<img src="gfx/bar.jpg" alt="Bar">
+<br>
+<br>
+<img src="gfx/logo.png" alt="Logo">
+</center>
+
+<p>La mejor clínica de chile</p>
+
+</body>
+
+<?html
+?>
+
+<center>
+<a href='#' class='button'>Hora</a>
+<a href='#' class='button'>Nosotros</a>
+<a href='#' class='button'>Especialidades</a>
+<a href='#' class='button'>Area Comercial</a>
+<a href='#' class='button'>Agendar Hora</a>
+<a href='#' class='button'>Contacto</a>
+</center>
+
+<h2>
+<center>
+Especialidades
+</center>
+</h2>
+
+<p>
+<center>
+Conoce nuestras especialidades, siempre enfocados en brindar un servicio personalizado.
+</center>
+</p>
+
+<center>
+<button type="button" class="collapsible">Ortodoncia</button>
+<div class="content">
+    <p>
+    Es el tratamiento de las malposiciones dentarias con aparatos fijos o removibles. 
+    Cuyo objetivo final es restaurar la posición correcta de los dientes para que cumplan 
+    su función adecuada.
+    </p>
+</div> 
+</center>
+
+<body>
+    <div class="wrapper">
+        <div class="collapsible">
+            <input type="checkbox" id="collapsible-head">
+        <label for="collapsible-head">Collapsible using only HTML</label>
+        <div class="collapsible-text">
+            <h2>Collapsible Heading</h2>
+                <p>
+                ASKJDHKJASHDKJHASKDHKSJADH
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+<p>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book. It has survived not only five centuries, but also the leap into
+electronic typesetting, remaining essentially unchanged. It was popularised in
+the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker 
+including versions of Lorem Ipsum.
+</p>
+
+</html>
